3e99b6e8ccf89b14abc898a851beaedfb8b9afef
Dmitriy Tomilov <d.tomilov@profsoft.pro>
2019-07-12 19:24:20 +0400

    add api login by password

--- src/Controller/Api/SecurityApiController.php
+++ src/Controller/Api/SecurityApiController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\EncoderFactory;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 
 class SecurityApiController extends AbstractApiController
 {
@@ -17,7 +19,7 @@ class SecurityApiController extends AbstractApiController
      * @param Request $request
      * @return Response
      *
-     * @Route("/registartion", name="api_login")
+     * @Route("/registartion", name="api_registration")
      */
     public function registrationAction(Request $request): Response
     {
@@ -36,6 +38,50 @@ class SecurityApiController extends AbstractApiController
 
             $json = $this->serializer->serialize(['uuid' => $user->getId(), 'token' => $device->getToken()],"json");
             return $this->createResponse($json);
+        } else {
+            throw new HttpException(Response::HTTP_FORBIDDEN, "Login failed");
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     *
+     * @Route("/login", name="api_login")
+     */
+    public function loginAction(Request $request): Response
+    {
+        $content = json_decode($request->getContent(), true);
+        if ($content != null) {
+            $repository = $this->getDoctrine()->getRepository(User::class);
+            $login = $content['login'];
+            $password = $content['password'];
+            if ($login != null && $password != null) {
+                /** @var User $user */
+                $user = $repository->findOneBy(['login' => $login]);
+                if ($user != null) {
+                    $defaultEncoder = new MessageDigestPasswordEncoder('sha512', true, 5000);
+                    $encoders = [User::class => $defaultEncoder];
+                    $encoderFactory = new EncoderFactory($encoders);
+                    $encoder = $encoderFactory->getEncoder($user);
+                    if ($encoder->encodePassword($password, $user->getSalt()) == $user->getPassword()) {
+                        $em = $this->getDoctrine()->getManager();
+                        $device = new Device();
+                        $device->setUser($user);
+                        $em->persist($device);
+                        $em->flush();
+
+                        $json = $this->serializer->serialize(['uuid' => $user->getId(), 'token' => $device->getToken()],"json");
+                        return $this->createResponse($json);
+                    } else {
+                        throw new HttpException(Response::HTTP_FORBIDDEN, "Login failed");
+                    }
+                } else {
+                    throw new HttpException(Response::HTTP_FORBIDDEN, "Login failed");
+                }
+            } else {
+                throw new HttpException(Response::HTTP_FORBIDDEN, "Login failed");
+            }
         } else {
             throw new HttpException(Response::HTTP_FORBIDDEN, "Login failed");
         }
