8aae99e120973e1dbd2c8286be0caf1ef30cbbcf
Dmitriy Tomilov <d.tomilov@profsoft.pro>
2019-07-12 19:43:07 +0400

    add temp user registration as login password

--- src/Controller/Api/UserApiController.php
+++ src/Controller/Api/UserApiController.php
@@ -4,8 +4,12 @@
 namespace App\Controller\Api;
 
 use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\EncoderFactory;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 
 /**
  * Class UserApiController
@@ -34,6 +38,36 @@ class UserApiController extends AbstractApiController
             return $this->createResponse($json);
         } else {
             return $this->createResponse(null);
+        }
+    }
+
+
+    /**
+     * @param Request $request
+     * @return Response
+     *
+     * @Route("/users/registration", methods={"PATCH"}, name="api_add_user_login")
+     */
+    public function addLoginToAuthUser(Request $request): Response
+    {
+        $content = json_decode($request->getContent(), true);
+
+        if ($this->getDoctrine()->getRepository(User::class)->findOneBy(['login' => $content['login']]) == null) {
+            $user = $this->getUser();
+            $user->setLogin($content['login']);
+            $user->setPassword($content['password']);
+            $defaultEncoder = new MessageDigestPasswordEncoder('sha512', true, 5000);
+            $encoders = [User::class => $defaultEncoder];
+            $encoderFactory = new EncoderFactory($encoders);
+            $encoder = $encoderFactory->getEncoder($user);
+            $user->setSalt(md5(time() . $user->getPassword()));
+            $user->setPassword($encoder->encodePassword($user->getPassword(), $user->getSalt()));
+
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+            return $this->createResponse(null);
+        } else {
+            throw new HttpException(499, "Login already exists");
         }
     }
 }
